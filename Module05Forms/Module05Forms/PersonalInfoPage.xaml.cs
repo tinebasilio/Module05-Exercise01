562463a955744c506f042bab8a59c46cbf303bbb
kbasi <kbasi@TINEBASILIO>
2024-05-04 02:07:49 +0800

    Add project files.

---
+++ Module05Forms/Module05Forms/PersonalInfoPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Module05Forms
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PersonalInfoPage : ContentPage
+    {
+        public PersonalInfoPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnToggle(object sender, ToggledEventArgs e)
+        {
+            MiddleName.IsEnabled = e.Value;
+        }
+
+        private void OnDateSelected(object sender, DateChangedEventArgs e)
+        {
+            var age = DateTime.Today.Year - e.NewDate.Year;
+            Age.Text = $"Age: {age}";
+        }
+    }
+}
